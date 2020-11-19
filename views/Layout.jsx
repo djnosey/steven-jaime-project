50e250a722ae2481ab8624f49d9a09c9ba1dca77
steven payne <djnosey@hotmail.co.uk>
2020-11-19 23:08:11 +0100

    make navbar

--- views/Layout.jsx
+++ views/Layout.jsx
@@ -6,15 +6,31 @@ function Layout(props) {
       <head>
         <meta charSet="utf-8" />
         <title> {props.title ? props.title : "My App"} </title>
+        <link
+          rel="stylesheet"
+          href="https://use.fontawesome.com/releases/v5.15.1/css/all.css"
+          integrity="sha384-vp86vTRFVJgpjF9jiIGPEEqYqlDwgyBgEF109VFjmqGmIY/Y4HV4d3Gp2irVfcrp"
+          crossorigin="anonymous"
+        />
+
         <link rel="stylesheet" href="/stylesheets/style.css" />
       </head>
 
       <body>
-      <nav id="navbarContainer">
-        <img src="" alt=""/>
-        <input type="text"/>
-      </nav>
-      {props.children}
+        <nav id="navbarContainer">
+          <i id="hamburgerIcon" class="fas fa-ellipsis-v"></i>
+          <div id="searchContainer">
+            <form action="">
+              <input
+                placeholder="search items..."
+                id="searchInput"
+                type="text"
+              />
+              <button id="searchBarButton"></button>
+            </form>
+          </div>
+        </nav>
+        {props.children}
       </body>
     </html>
   );
