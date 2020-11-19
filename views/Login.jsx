ae1544f616a64d1739cbe0514a29a6ac0fd0441c
steven payne <djnosey@hotmail.co.uk>
2020-11-19 23:10:29 +0100

    add layout to login signup

--- views/Login.jsx
+++ views/Login.jsx
@@ -1,18 +1,21 @@
 const React = require("react");
+const Layout = require("./Layout");
 
 function Login(props) {
   return (
-    <form action="/auth/login" method="post">
-      <input type="text" name="username" placeholder="username" />
-      <br />
-      <input type="password" name="password" placeholder="password" />
-      <br />
-      <button type="submit">Click me</button>
-      <div id="errorMessages"></div>
-      <p>
-        Don't have an account?<a href="/auth/signup">Click here</a>
-      </p>
-    </form>
+    <Layout>
+      <form action="/auth/login" method="post">
+        <input type="text" name="username" placeholder="username" />
+        <br />
+        <input type="password" name="password" placeholder="password" />
+        <br />
+        <button type="submit">Click me</button>
+        <div id="errorMessages"></div>
+        <p>
+          Don't have an account?<a href="/auth/signup">Click here</a>
+        </p>
+      </form>
+    </Layout>
   );
 }
 
--- views/Signup.jsx
+++ views/Signup.jsx
@@ -1,20 +1,23 @@
 const React = require("react");
+const Layout = require("./Layout");
 
 function Signup(props) {
   return (
-    <form action="/auth/signup" method="post">
-      <input type="text" name="username" placeholder="username" />
-      <br />
-      <input type="password" name="password" placeholder="password" />
-      <br />
-      <input
-        type="password"
-        name="repeatPassword"
-        placeholder="re-type password"
-      />
-      <button type="submit">Create an account!</button>
-      <div id="errorMessages">{props.errorMessage}</div>
-    </form>
+    <Layout>
+      <form action="/auth/signup" method="post">
+        <input type="text" name="username" placeholder="username" />
+        <br />
+        <input type="password" name="password" placeholder="password" />
+        <br />
+        <input
+          type="password"
+          name="repeatPassword"
+          placeholder="re-type password"
+        />
+        <button type="submit">Create an account!</button>
+        <div id="errorMessages">{props.errorMessage}</div>
+      </form>
+    </Layout>
   );
 }
 
