8129132eff3306ff01f81e24fe5b2f0d82b417f4
JaimePintoP <jaimepinto.saxo@gmail.com>
2020-11-19 15:02:55 +0100

    models done

---
+++ models/Product.model.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+//Create Schema
+
+const productSchema = new Schema(
+  {
+    name: { type: String },
+    category: { type: String },
+    description: { type: String },
+    image: String,
+    condition: { type: Number },
+    seller: [{ type: mongoose.SchemaType.ObjectId, ref: "User" }],
+  },
+  {
+    timestamps: {
+      createdAt: "created_at",
+      updatedAt: "updated_at",
+    },
+  }
+);
+
+// Create the model
+const Product = mongoose.model("Product", productSchema);
+
+// Exports
+module.exports = Product;
--- models/User.model.js
+++ models/User.model.js
@@ -1,1 +1,28 @@
 // Model
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+//Create Schema
+
+const userSchema = new Schema(
+  {
+    name: { type: String, unique: true },
+    password: { type: String },
+    image: String,
+    transactions: [{ type: mongoose.SchemaType.ObjectId, ref: "Product" }],
+    myItems: [{ type: mongoose.SchemaType.ObjectId, ref: "Product" }],
+    messages: [{ type: mongoose.SchemaType.ObjectId, ref: "Product" }],
+  },
+  {
+    timestamps: {
+      createdAt: "created_at",
+      updatedAt: "updated_at",
+    },
+  }
+);
+
+// Create the model
+const User = mongoose.model("User", userSchema);
+
+// Exports
+module.exports = User;
