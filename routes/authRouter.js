9e1641a48836da4f2d403344a146b22eccc2fa15
JaimePintoP <jaimepinto.saxo@gmail.com>
2020-11-19 19:54:31 +0100

    Add Log In logic in authRouter

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -11,6 +11,8 @@ const saltRounds = 10;
 
 // Your routes
 
+// SIGN UP  -----------
+
 // GET     /auth/signup
 authRouter.get("/signup", (req, res, next) => {
   res.render("Signup");
@@ -37,25 +39,62 @@ authRouter.post("/signup", (req, res, next) => {
   }
 
   User.findOne({ username: username })
-  .then((user) => {
-    if (user) {
-      const props = { errorMessage: "User already exists" };
-      res.render("Signup", props);
+    .then((user) => {
+      if (user) {
+        const props = { errorMessage: "User already exists" };
+        res.render("Signup", props);
+        return;
+      }
+
+      // if user available, encrypt
+      const salt = bcrypt.genSaltSync(saltRounds);
+      const hashedPassword = bcrypt.hashSync(password, salt);
+
+      // After encrypting the password, create the new user in DB
+      User.create({ username, password: hashedPassword })
+        .then((createdUser) => {
+          res.redirect("/");
+        })
+        .catch((err) => console.log(err));
+    })
+    .catch((err) => console.log(err));
+});
+
+// LOG IN ----------
+
+// GET  /auth/login  - Render the Login form
+authRouter.get("/login", (req, res, next) => {
+  res.render("Login");
+});
+
+// POST     /auth/login
+authRouter.post("/login", (req, res, next) => {
+  const { username, password } = req.body;
+
+  if (username === "" || password === "") {
+    const props = { errorMessage: "Please enter your username and password" };
+    res.render("Login", props);
+    return;
+  }
+
+  User.findOne({ username }).then((user) => {
+    if (!user) {
+      const props = { errorMessage: "User not found" };
+      res.render("Login", props);
       return;
     }
 
-    // if user available, encrypt
-    const salt = bcrypt.genSaltSync(saltRounds);
-    const hashedPassword = bcrypt.hashSync(password, salt);
+    const passwordCorrect = bcrypt.compareSync(password, user.password);
 
-    // After encrypting the password, create the new user in DB
-    User.create( {username, password: hashedPassword})
-        .then( (createdUser)=>{
-            res.redirect("/")
-        })
-        .catch( (err) => console.log(err));
-  })
-  .catch( (err) => console.log(err));
+    if (passwordCorrect) {
+      req.session.currentUser = user;
+      res.redirect("/");
+    } else {
+      res.render("Login", { errorMessage: "Incorrect password" });
+    }
+  });
 });
+
+// GET     /auth/logout
 
 module.exports = authRouter;
--- views/Signup.jsx
+++ views/Signup.jsx
@@ -3,12 +3,16 @@ const React = require("react");
 function Signup(props) {
   return (
     <form action="/auth/signup" method="post">
-      <input type="text" name="username" />
+      <input type="text" name="username" placeholder="username" />
       <br />
-      <input type="password" name="password" />
+      <input type="password" name="password" placeholder="password" />
       <br />
-      <input type="password" name="repeatPassword" />
-      <button type="submit">click me</button>
+      <input
+        type="password"
+        name="repeatPassword"
+        placeholder="re-type password"
+      />
+      <button type="submit">Create an account!</button>
       <div id="errorMessages">{props.errorMessage}</div>
     </form>
   );
