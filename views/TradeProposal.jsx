390cf80deea2df00e2e42e0d82f88f6d01668028
steven payne <djnosey@hotmail.co.uk>
2020-11-26 18:22:10 +0100

    style trade proposal

--- views/TradeProposal.jsx
+++ views/TradeProposal.jsx
@@ -6,42 +6,49 @@ function TradeProposal(props) {
   console.log(props);
   return (
     <Layout>
-      <h3>Trade offer</h3>
-      <h4>
-        The user {props.proposedProduct.seller.username} wants to trade with you
-        his product:
-      </h4>
-      <ProductCard
-        key={props.proposedProduct._id}
-        image={props.proposedProduct.image}
-        title={props.proposedProduct.name}
-        condition={props.proposedProduct.condition}
-        category={props.proposedProduct.category}
-        id={props.proposedProduct._id}
-      />
-      <h4>for your</h4>
-      <ProductCard
-        key={props.yourProduct._id}
-        image={props.yourProduct.image}
-        title={props.yourProduct.name}
-        condition={props.yourProduct.condition}
-        category={props.yourProduct.category}
-        id={props.yourProduct._id}
-      />
-      <h4>Do you accept the trade?</h4>
-
-      <form
-        action={`/transaction/tradeDone?proposedproduct=${props.proposedProduct._id}&yourproduct=${props.yourProduct._id}`}
-        method="POST"
-      >
-        <button className="formButton">yes</button>
-      </form>
-      <form
-        action={`/transaction/tradeRejected?proposedproduct=${props.proposedProduct._id}&yourproduct=${props.yourProduct._id}`}
-        method="POST"
-      >
-        <button className="formButton">no</button>
-      </form>
+      <div className="trade-proposal-container">
+        <h3>Trade offer</h3>
+        <h4>
+          The user {props.proposedProduct.seller.username} wants to trade with
+          you his product:
+        </h4>
+        <div className="trade-proposal-card">
+          <ProductCard
+            key={props.proposedProduct._id}
+            image={props.proposedProduct.image}
+            title={props.proposedProduct.name}
+            condition={props.proposedProduct.condition}
+            category={props.proposedProduct.category}
+            id={props.proposedProduct._id}
+          />
+        </div>
+        <h4>for your</h4>
+        <div className="trade-proposal-card">
+          <ProductCard
+            key={props.yourProduct._id}
+            image={props.yourProduct.image}
+            title={props.yourProduct.name}
+            condition={props.yourProduct.condition}
+            category={props.yourProduct.category}
+            id={props.yourProduct._id}
+          />
+        </div>
+        <h4>Do you accept the trade?</h4>
+        <div className="trade-proposal-buttons">
+          <form
+            action={`/transaction/tradeRejected?proposedproduct=${props.proposedProduct._id}&yourproduct=${props.yourProduct._id}`}
+            method="POST"
+          >
+            <button className="badactionbutton">no</button>
+          </form>
+          <form
+            action={`/transaction/tradeDone?proposedproduct=${props.proposedProduct._id}&yourproduct=${props.yourProduct._id}`}
+            method="POST"
+          >
+            <button className="formButton">yes</button>
+          </form>
+        </div>
+      </div>
     </Layout>
   );
 }
