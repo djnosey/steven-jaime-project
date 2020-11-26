cfbd0e4015bb864e7529ce070ea093be6c52268a
steven payne <djnosey@hotmail.co.uk>
2020-11-26 17:13:28 +0100

    style edit profile page

--- views/EditProfile.jsx
+++ views/EditProfile.jsx
@@ -4,43 +4,58 @@ const Layout = require("./Layout");
 function EditProfile(props) {
   return (
     <Layout>
-      <h2>Edit Profile</h2>
-      <h3>{props.actualUser.username}</h3>
-      <img src={props.actualUser.image} alt="" />
-      <form
-        action={`/user/editprofile?userid=${props.actualUser._id}`}
-        method="POST"
-        encType="multipart/form-data"
-      >
-        <label>Change username:</label>
-        <input
-          type="text"
-          name="username"
-          placeholder="username"
-          defaultValue={props.actualUser.username}
-        />
-        <label>Change phone number:</label>
-        <input
-          type="number"
-          name="phone"
-          placeholder="653 622 489"
-          defaultValue={props.actualUser.phone}
-        />
-        <label>Change profile picture</label>
-        <input className="formButton" type="file" name="profilepic" />
-        <button className="formButton" type="submit">
-          Save
-        </button>
-      </form>
+      <div className="edit-profile-container">
+        <h2>Edit Profile</h2>
+        <div className="edit-image-name">
+          <img className="sellerimg" src={props.actualUser.image} alt="" />
+          <h3>{props.actualUser.username}</h3>
+        </div>
+        <form
+          id="edit-info-form"
+          action={`/user/editprofile?userid=${props.actualUser._id}`}
+          method="POST"
+          encType="multipart/form-data"
+        >
+          <input
+            className="forminput"
+            type="text"
+            name="username"
+            placeholder="username"
+            defaultValue={props.actualUser.username}
+          />
+          <input
+            className="forminput"
+            type="number"
+            name="phone"
+            placeholder="653 622 489"
+            defaultValue={props.actualUser.phone}
+          />
+          <div className="change-pic-div">
+            <div className="addfile">
+              <label className="addProfilePicture">
+                Change profile picture
+              </label>
+              <input type="file" name="profilepic" />
+            </div>
+          </div>
+          <button className="formButton" type="submit">
+            Save
+          </button>
+        </form>
 
-      <form
-        action={`/user/delete?userid=${props.actualUser._id}`}
-        method="POST"
-      >
-        <button type="submit">Delete Account</button>
-      </form>
+        <div className="edit-profile-buttons">
+          <form
+            action={`/user/delete?userid=${props.actualUser._id}`}
+            method="POST"
+          >
+            <button className="badactionbutton" type="submit">
+              Delete Account
+            </button>
+          </form>
 
-      <a href="/user/profile">Back to your profile</a>
+          <a href="/user/profile">Back to your profile</a>
+        </div>
+      </div>
     </Layout>
   );
 }
